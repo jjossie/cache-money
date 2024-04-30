667377186174c993d994a6b05ffe0e2f2989313e
rbandamede <rishika.bandamede@gmail.com>
2024-04-30 14:44:48 -0400

    chat button

--- cache-money.angular/src/app/app.component.ts
+++ cache-money.angular/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { PanelModule } from 'primeng/panel'
 import { NavTabsComponent } from './components/nav-tabs/nav-tabs.component';
+import { ChatFooterComponent } from './components/chat-footer/chat-footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule, PanelModule, NavTabsComponent],
+  imports: [RouterOutlet, ButtonModule, PanelModule, NavTabsComponent, ChatFooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

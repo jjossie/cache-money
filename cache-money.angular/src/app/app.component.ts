0db4259aecdc2594c4649f0f0e07993e47f4d744
rbandamede <rishika.bandamede@gmail.com>
2024-04-30 15:48:02 -0400

    nudge card

--- cache-money.angular/src/app/app.component.ts
+++ cache-money.angular/src/app/app.component.ts
@@ -4,14 +4,16 @@ import { ButtonModule } from 'primeng/button';
 import { PanelModule } from 'primeng/panel'
 import { NavTabsComponent } from './components/nav-tabs/nav-tabs.component';
 import { ChatFooterComponent } from './components/chat-footer/chat-footer.component';
+import { NudgeCardComponent } from './components/nudge-card/nudge-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule, PanelModule, NavTabsComponent, ChatFooterComponent],
+  imports: [RouterOutlet, ButtonModule, PanelModule, NavTabsComponent, ChatFooterComponent, NudgeCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'cache-money.angular';
+  content="Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet"
 }
---
+++ cache-money.angular/src/app/components/nudge-card/nudge-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input} from '@angular/core';
+import { CardModule } from 'primeng/card';
+
+
+@Component({
+  selector: 'app-nudge-card',
+  standalone: true,
+  imports: [CardModule],
+  templateUrl: './nudge-card.component.html',
+  styleUrl: './nudge-card.component.scss'
+})
+export class NudgeCardComponent {
+  @Input() nudgeContent : string = '';
+
+}

e6645f20bc2601d21037fc52569268b633ea30d2
rbandamede <rishika.bandamede@gmail.com>
2024-05-01 15:23:07 -0400

    nudges and cleanup

--- cache-money.angular/src/app/pages/transactions/transactions.component.ts
+++ cache-money.angular/src/app/pages/transactions/transactions.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Transaction } from '../../models/transaction';
 import { Account } from '../../models/account';
 import { CacheMoneyApiService } from '../../services/cache-money-api.service';
-import { Observable } from 'rxjs';
+import { Observable, Subject, of } from 'rxjs';
 
 @Component({
     selector: 'app-transactions',
@@ -17,7 +17,15 @@ export class TransactionsComponent {
         { name: "Investment return", amount: 50, category: "Income", account: "Savings" },
         { name: "Movie ticket", amount: 15, category: "Entertainment", account: "Credit card" },
       ]
+
+    categories :any[]= ['Food','Transport','Housing', 'Entertainment']
+
+    // categories$: Subject<string[]>
+    actualSpend = [180,280,100,320,330]
+    suggestedSpend = [200,200,300,200,300]
+
       constructor(private api: CacheMoneyApiService) {
+        // this.categories$ = new Subject();
         this.api.getCurrentSpending("Doug").subscribe(currentSpending => {
           this.transactions = currentSpending.transactions
           .map(transaction => {
@@ -33,10 +41,18 @@ export class TransactionsComponent {
             const date = new Date(transaction.date)
             return date.getMonth() === new Date().getMonth();
           })
-          .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
-          ;
+          .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+
+        //   this.categories$.next(Object.keys(currentSpending.categories) || ['']);
+          this.categories = Object.keys(currentSpending.categories);
+          console.log(this.categories);
+          
         });
+
+        
+
       }
+      
     accounts: Account[] = [
         {
           name: "Checking Account",
@@ -52,6 +68,19 @@ export class TransactionsComponent {
           name: "Credit Card",
           value: -100.00,  // Negative value for liability
           type: "Liability"
-        }
+        },
+        {
+            name: "Vanguard Brokerage Account",
+            value: 55000,  // Negative value for liability
+            type: "Investments"
+          },
+          {
+            name: "Fideliti",
+            value: 13000,  // Negative value for liability
+            type: "Investments"
+          }
       ];
+
+      
 }
+

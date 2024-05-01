667e952ea18546fd9c477558fdb64f9695f79048
Joel Jossie <joeljossie@gmail.com>
2024-05-01 14:49:50 -0400

    some chatting works!

--- cache-money.angular/src/app/components/chat/chat-conversation/chat-conversation.component.ts
+++ cache-money.angular/src/app/components/chat/chat-conversation/chat-conversation.component.ts
@@ -8,21 +8,12 @@ import { ChatMessage } from '../../../models/chat/chat';
   styleUrl: './chat-conversation.component.scss'
 })
 export class ChatConversationComponent {
-  messages: ChatMessage[] = [
-    {
-      text: "Test message one",
-      sent: true,
-      time: "now"
-    },
-    {
-      text: "Bum bum bay ay ay ay ay ay eee eee how you feelin how you been? bum bum bumb bow ay ay ay ay cant stop us now woooooooo",
-      sent: false,
-      time: "now"
-    },
-    {
-      text: "for all my southside fellas that know me best i feel like chatGPT might still be dumb at math, why i made that AI famous 🗿",
-      sent: true,
-      time: "now"
-    },
-  ]
+  
+  messages: ChatMessage[] = [];
+
+  onNewMessage(message: ChatMessage) {
+    this.messages.push(message);
+  }
+
+
 }
--- cache-money.angular/src/app/components/chat/chat-input/chat-input.component.ts
+++ cache-money.angular/src/app/components/chat/chat-input/chat-input.component.ts
@@ -1,18 +1,33 @@
-import { Component } from '@angular/core';
-
+import { Component, EventEmitter, Output } from '@angular/core';
+import { CacheMoneyApiService } from '../../../services/cache-money-api.service';
+import { ChatMessage } from '../../../models/chat/chat';
 
 @Component({
   selector: 'app-chat-input',
   templateUrl: './chat-input.component.html',
-  styleUrl: './chat-input.component.scss'
+  styleUrl: './chat-input.component.scss',
 })
 export class ChatInputComponent {
-  chatValue: string | undefined;
+  chatValue = '';
 
-  sendChat() { 
-    console.log(this.chatValue)
+  @Output()
+  newMessageEvent = new EventEmitter<ChatMessage>();
+
+  constructor(private api: CacheMoneyApiService) {
     this.chatValue = '';
-    console.log(this.chatValue.length, this.chatValue, typeof(this.chatValue));
   }
 
+  sendChat() {
+    console.log(this.chatValue);
+  this.newMessageEvent.emit({text: this.chatValue, sent: true});
+
+    this.api.sendChatMessage(this.chatValue, 'Doug').subscribe((data) => {
+      this.newMessageEvent.emit({text: `${data}`, sent: false});
+      console.log(data);
+    });
+
+    this.chatValue = '';
+    console.log(this.chatValue.length, this.chatValue, typeof this.chatValue);
+
+  }
 }

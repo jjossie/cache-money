dcc4a80d1bca69d36099edbd9052e34b6fd4d39c
Joel Jossie <joeljossie@gmail.com>
2024-05-01 16:15:36 -0400

    final tweaks

--- cache-money.angular/src/app/components/spending-bar-chart/spending-bar-chart.component.ts
+++ cache-money.angular/src/app/components/spending-bar-chart/spending-bar-chart.component.ts
@@ -7,8 +7,6 @@ import { Component, Input } from '@angular/core';
 })
 export class SpendingBarChartComponent {
 
-  //spendCategories: string[] = ['Housing','Transportation','Groceries','Entertainment']
-    @Input() spendCategories!: string[];
   @Input() actualSpend!: number[];
   @Input() suggestedSpend!: number[];
   @Input() spendCategories !: string[];
